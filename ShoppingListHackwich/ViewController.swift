88011ebaed0ea61b9c03a403643aac43055e4b50
Kenneth Johnson <kd123johnson@gmail.com>
2022-01-05 11:55:27 -0700

    Tableview

--- ShoppingListHackwich/ViewController.swift
+++ ShoppingListHackwich/ViewController.swift
@@ -7,13 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var tableViewA: UITableView!
+    
+    
+    
+    
 
-
+    
+    
 }
 
