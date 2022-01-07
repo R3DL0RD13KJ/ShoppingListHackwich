a8e78b511156b13398f25f7989e23320c2f1aa64
Kenneth Johnson <kd123johnson@gmail.com>
2022-01-07 11:17:06 -0700

    Shoppinglist 1.0

--- ShoppingListHackwich/ViewController.swift
+++ ShoppingListHackwich/ViewController.swift
@@ -7,14 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITableViewDataSource
 {
-
     override func viewDidLoad()
     {
+        tableViewA.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableViewA.dequeueReusableCell(withIdentifier: "Cellld", for: indexPath)
+        cell.textLabel?.text = "hello"
+        return cell
+    }
+    
     @IBOutlet weak var tableViewA: UITableView!
     
     
