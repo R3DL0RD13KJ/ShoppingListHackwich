2844a476f39969066451869654c01726d945bf3f
Kenneth Johnson <kd123johnson@gmail.com>
2022-01-07 11:22:23 -0700

    shopping list 2.0

---
+++ ShoppingListHackwich/Item.swift
@@ -0,0 +1,26 @@
+//
+//  Item.swift
+//  ShoppingListHackwich
+//
+//  Created by Kenneth Johnson on 1/7/22.
+//
+
+import Foundation
+
+class Item
+{
+    var name: String
+    
+    init(name:String)
+    {
+        self.name = name
+    }
+}
+
+
+
+
+
+
+
+
